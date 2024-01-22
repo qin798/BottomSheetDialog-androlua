d07bf39e6cd091bc27fdeccea8b96229f6da670a
秦人 <121938003+qin798@users.noreply.github.com>
2024-01-22 18:54:04 +0800

    Add files via upload

---
+++ mods/dialog/bottomsheetdialog/dialog_list.lua
@@ -0,0 +1,9 @@
+local ListView = bindClass "android.widget.ListView"
+
+return {
+  ListView,
+  layout_width="match",
+  layout_height="match",
+  id="mListView",
+  dividerHeight="0dp",
+}
---
+++ mods/dialog/bottomsheetdialog/dialog_listitem.lua
@@ -0,0 +1,23 @@
+local ColorStateList = bindClass "android.content.res.ColorStateList"
+local MaterialButton = bindClass "com.google.android.material.button.MaterialButton"
+local MaterialCardView = bindClass "com.google.android.material.card.MaterialCardView"
+local LinearLayoutCompat = bindClass "androidx.appcompat.widget.LinearLayoutCompat"
+local MaterialTextView = bindClass "com.google.android.material.textview.MaterialTextView"
+local AppCompatImageView = bindClass "androidx.appcompat.widget.AppCompatImageView"
+local ColorBackground = LuaThemeUtil.getColorSurfaceVariant();
+local ColorPrimary = LuaThemeUtil.getColorPrimary();
+local ColorText = LuaThemeUtil.getColorOnSurfaceVariant();
+local ColorRipple = LuaThemeUtil.getColorSecondaryVariant();
+
+return {
+  MaterialButton,
+  layout_width="match",
+  layout_height="52dp",
+  BackgroundTintList=ColorStateList.valueOf(ColorBackground),
+  id="button",
+  textColor=ColorText,
+  iconTint=ColorStateList.valueOf(ColorPrimary),
+  RippleColor=ColorStateList.valueOf(ColorRipple),
+  --textAllCaps=false,
+  gravity="start|center",
+}
